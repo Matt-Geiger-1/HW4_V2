381bb2375235278253a464d437e30ff45a1e354b
Matt-Geiger-1 <matt.geiger@kellogg.northwestern.edu>
2026-03-18 01:59:09 +0000

    final stage

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,8 +14,20 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["entry"]["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = current_user["id"]
-    @entry.save
 
+    uploaded_file = params["entry"]["image"]
+    if uploaded_file != nil
+      filename = SecureRandom.hex + "_" + uploaded_file.original_filename
+      path = Rails.root.join("public", "uploads", filename)
+
+      File.open(path, "wb") do |file|
+        file.write(uploaded_file.read)
+      end
+
+      @entry["image"] = filename
+    end
+
+    @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
 end
